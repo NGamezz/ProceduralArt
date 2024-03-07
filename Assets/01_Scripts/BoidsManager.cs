9b5da663dd8b95354713a3f0a2f1489531f56ebb
NGamezz <ninjapro480000@gmail.com>
2024-03-07 01:37:40 +0100

    Multi-Threaded Boids in Progress.

--- Assets/01_Scripts/BoidsManager.cs
+++ Assets/01_Scripts/BoidsManager.cs
@@ -1,8 +1,8 @@
-using System;
-using System.Collections;
 using System.Collections.Generic;
-using System.Threading;
 using System.Threading.Tasks;
+using Unity.Burst;
+using Unity.Collections;
+using Unity.Jobs;
 using Unity.Mathematics;
 using UnityEngine;
 
@@ -11,9 +11,24 @@ public class BoidsManager : MonoBehaviour
     [SerializeField] private int boidsCount = 50;
     [SerializeField] private GameObject boidsPrefab;
 
+    [SerializeField] private float seperationDistance = 2.0f;
+    [SerializeField] private float seperationStrenght = 2.0f;
+
+    [SerializeField] private float targetWeight = 100.0f;
+
+    [SerializeField] private float allignmentMultiplier = 0.1f;
+
+    [SerializeField] private Transform target;
+
+    [SerializeField] private float2 minMaxSpeed;
+
+    [SerializeField] private float cohesionStrenght = 2.0f;
+
+    [SerializeField] private float maxSteerForce = 4.0f;
+
     [SerializeField] private float2 bounds;
 
-    private float seperationDistance = 10.0f;
+    private float chunkDistance = 50.0f;
 
     private List<Cluster> clusters = new();
     private List<Transform> transforms = new();
@@ -21,11 +36,9 @@ public class BoidsManager : MonoBehaviour
 
     [SerializeField] private Gradient colourGradient;
 
-    private async void Start()
+    private void Start ()
     {
-        Cluster defaultCluster = new Cluster();
-
-        clusters.Add(defaultCluster);
+        Cluster defaultCluster = new(0);
 
         for ( int i = 0; i < boidsCount; i++ )
         {
@@ -34,123 +47,337 @@ public class BoidsManager : MonoBehaviour
             gameObject.transform.position = position;
             transforms.Add(gameObject.transform);
 
-            var boid = new Boid(i, position, defaultCluster);
+            var boid = new Boid(i, position, gameObject.transform.forward);
 
             defaultCluster.boids.Add(boid);
             boids.Add(boid);
         }
 
-        await CheckClusters();
+        defaultCluster = CalculateFlockData(defaultCluster);
+        clusters.Add(defaultCluster);
 
-        int count = 0;
+        //await CheckClusters();
+
+        //int count = 0;
+        //for ( int i = 0; i < clusters.Count; i++ )
+        //{
+        //    var cluster = clusters[i];
+
+        //    CalculateFlockData(ref cluster);
+
+        //    Debug.Log(cluster.boids.Count);
+        //    foreach ( var boid in cluster.boids )
+        //    {
+        //        transforms[boid.index].GetComponent<MeshRenderer>().material.color = colourGradient.Evaluate(Mathf.InverseLerp(0.0f, boidsCount, count));
+        //    }
+        //    count++;
+        //}
+    }
+
+    //////Optimize That.
+    //private async Task CheckClusters ()
+    //{
+    //    await Awaitable.BackgroundThreadAsync();
+
+    //    for ( int i = 0; i < boids.Count; i++ )
+    //    {
+    //        var currentBoid = boids[i];
+
+    //        CalculateFlockData(ref currentBoid.currentCluster);
+
+    //        var distanceFirst = Vector3.Distance(currentBoid.currentCluster.flockPosition / currentBoid.currentCluster.boids.Count, currentBoid.position);
+
+    //        if ( distanceFirst > chunkDistance )
+    //        {
+    //            bool hasPlace = false;
+    //            for ( int t = 0; t < clusters.Count; t++ )
+    //            {
+    //                var cluster = clusters[t];
+    //                CalculateFlockData(ref cluster);
+
+    //                var distance = Vector3.Distance(cluster.flockPosition / cluster.boids.Count, currentBoid.position);
+    //                if ( distance < chunkDistance )
+    //                {
+    //                    currentBoid.currentCluster.boids.Remove(currentBoid);
+    //                    currentBoid.currentCluster = cluster;
+    //                    currentBoid.currentCluster.boids.Add(currentBoid);
+
+    //                    hasPlace = true;
+    //                }
+    //            }
+
+    //            if ( !hasPlace )
+    //            {
+    //                currentBoid.currentCluster.boids.Remove(currentBoid);
+    //                currentBoid.currentCluster = new Cluster();
+    //                currentBoid.currentCluster.boids.Add(currentBoid);
+    //                clusters.Add(currentBoid.currentCluster);
+    //            }
+    //        }
+    //    }
+    //}
+
+    private void UpdateClusters ()
+    {
+        for ( int i = 0; i < clusters.Count; i++ )
+        {
+            UpdateCluster(clusters[i]);
+        }
+    }
+
+    private void Update ()
+    {
+        UpdateClusters();
+    }
+
+    [BurstCompile]
+    private Cluster CalculateFlockData ( Cluster cluster )
+    {
+        NativeArray<Vector3> positions = new(cluster.boids.Length, Allocator.TempJob);
+        NativeArray<Vector3> directions = new(cluster.boids.Length, Allocator.TempJob);
+        NativeArray<Boid> boids = new(cluster.boids.Length, Allocator.TempJob);
+
+        CalculateClusterData calculateClusterData = new()
+        {
+            seperationDistance = seperationDistance,
+            boids = boids,
+            position = positions,
+            direction = directions,
+            cluster = cluster
+        };
+
+        calculateClusterData.Schedule(cluster.boids.Length, 64).Complete();
+
+        Cluster newCluster = new(0);
+        newCluster.boids.AddRange(calculateClusterData.boids);
+
+        for ( int i = 0; i < calculateClusterData.boids.Length; i++ )
+        {
+            newCluster.flockPosition += calculateClusterData.position[i];
+            newCluster.flockDirection += calculateClusterData.direction[i];
+        }
+
+        calculateClusterData.boids.Dispose();
+        calculateClusterData.direction.Dispose();
+        calculateClusterData.position.Dispose();
+        cluster.boids.Dispose();
+
+        return newCluster;
+    }
+
+    private void OnDisable ()
+    {
         foreach ( var cluster in clusters )
         {
-            Debug.Log(cluster.boids.Count);
-            foreach(var boid in cluster.boids)
-            {
-                transforms[boid.index].GetComponent<MeshRenderer>().material.color = colourGradient.Evaluate(Mathf.InverseLerp(0.0f, boidsCount, count));
-            }
-            count++;
+            cluster.boids.Dispose();
         }
     }
 
-    private Vector3 GetAveragePosition ( Cluster cluster )
+    [BurstCompile]
+    private void UpdateCluster ( Cluster cluster )
     {
-        Vector3 velocity = Vector3.zero;
-
-        foreach ( var boid in cluster.boids )
+        clusters.Remove(cluster);
+        cluster = CalculateFlockData(cluster);
+        for ( int i = 0; i < cluster.boids.Length; i++ )
         {
-            velocity += boid.position;
+            cluster.boids[i] = UpdateBoid(cluster.boids[i], cluster);
         }
-
-        return velocity / cluster.boids.Count;
+        clusters.Add(cluster);
     }
 
-    private Vector3 GetAverageVelocity ( Cluster cluster )
-    {
-        Vector3 velocity = Vector3.zero;
+    //[BurstCompile]
+    //private struct UpdateBoidJob : IJobParallelFor
+    //{
+    //    public NativeArray<Boid> boids;
+    //    [ReadOnly] public Cluster cluster;
 
-        foreach ( var boid in cluster.boids )
+    //    [ReadOnly] public float deltaTime;
+    //    [ReadOnly] public float2 minMaxSpeed;
+    //    [ReadOnly] public float maxSteerForce;
+    //    [ReadOnly] public float targetWeight;
+    //    [ReadOnly] public float cohesionStrenght;
+    //    [ReadOnly] public float allignmentMultiplier;
+    //    [ReadOnly] public float seperationStrenght;
+
+    //    [WriteOnly] public NativeArray<Vector3> positions;
+    //    [WriteOnly] public NativeArray<Vector3> directions;
+
+    //    public void Dispose ()
+    //    {
+    //        positions.Dispose();
+    //        directions.Dispose();
+    //        boids.Dispose();
+    //    }
+
+    //    public void Execute ( int index )
+    //    {
+    //        var boid = cluster.boids[index];
+    //        Vector3 acceleration = Vector3.zero;
+
+    //        //if ( target != null )
+    //        //{
+    //        //    var direction = target.position - boid.position;
+    //        //    var offSet = SteerTowards(direction, boid.Velocity) * targetWeight;
+
+    //        //    acceleration += offSet;
+    //        //}
+
+    //        if ( cluster.boids.Length != 0 )
+    //        {
+    //            var flockCentre = cluster.flockPosition / cluster.boids.Length;
+
+    //            Vector3 offSetToFlockCentre = flockCentre - boid.position;
+
+    //            var alignmentForce = SteerTowards(cluster.flockDirection.normalized, boid.Velocity) * allignmentMultiplier;
+    //            var cohesionForce = SteerTowards(offSetToFlockCentre, boid.Velocity) * cohesionStrenght;
+    //            var seperationForce = SteerTowards(boid.avoidanceHeading, boid.Velocity) * seperationStrenght;
+
+    //            acceleration += alignmentForce + cohesionForce + seperationForce;
+    //        }
+
+    //        boid.Velocity += acceleration * Time.deltaTime;
+
+    //        float speed = boid.Velocity.magnitude;
+    //        var dir = boid.Velocity / speed;
+    //        speed = math.clamp(speed, minMaxSpeed.x, minMaxSpeed.y);
+    //        boid.Velocity = dir * speed;
+
+    //        boid.position += boid.Velocity * deltaTime;
+    //        boid.forward = dir;
+
+    //        boids[index] = boid;
+    //        positions[index] = boid.position;
+    //        directions[index] = boid.forward;
+    //    }
+
+    //    private readonly Vector3 SteerTowards ( Vector3 vector, Vector3 velocity )
+    //    {
+    //        Vector3 v = vector.normalized * minMaxSpeed.y - velocity;
+    //        return Vector3.ClampMagnitude(v, maxSteerForce);
+    //    }
+    //}
+
+    private Boid UpdateBoid ( Boid boid, Cluster cluster )
+    {
+        Vector3 acceleration = Vector3.zero;
+
+        if ( target != null )
         {
-            velocity += boid.Velocity;
+            var direction = target.position - boid.position;
+            var offSet = SteerTowards(direction, boid.Velocity) * targetWeight;
+
+            acceleration += offSet;
         }
 
-        return velocity / cluster.boids.Count;
-    }
-
-    //Optimize That.
-    private async Task CheckClusters ()
-    {
-        await Awaitable.BackgroundThreadAsync();
-
-        for ( int i = 0; i < boids.Count; i++ )
+        if ( cluster.boids.Length != 0 )
         {
-            var currentBoid = boids[i];
+            var flockCentre = cluster.flockPosition / cluster.boids.Length;
 
-            var distanceFirst = Vector3.Distance(GetAveragePosition(currentBoid.currentCluster), currentBoid.position);
-            if ( distanceFirst > seperationDistance )
-            {
-                bool hasPlace = false;
-                foreach ( var cluster in clusters )
-                {
-                    var distance = Vector3.Distance(GetAveragePosition(cluster), currentBoid.position);
-                    if ( distance < seperationDistance )
-                    {
-                        currentBoid.currentCluster.boids.Remove(currentBoid);
-                        currentBoid.currentCluster = cluster;
-                        currentBoid.currentCluster.boids.Add(currentBoid);
+            Vector3 offSetToFlockCentre = flockCentre - boid.position;
 
-                        hasPlace = true;
-                    }
-                }
+            var alignmentForce = SteerTowards(cluster.flockDirection.normalized, boid.Velocity) * allignmentMultiplier;
+            var cohesionForce = SteerTowards(offSetToFlockCentre, boid.Velocity) * cohesionStrenght;
+            var seperationForce = SteerTowards(boid.avoidanceHeading, boid.Velocity) * seperationStrenght;
 
-                if ( !hasPlace )
-                {
-                    currentBoid.currentCluster.boids.Remove(currentBoid);
-                    currentBoid.currentCluster = new Cluster();
-                    currentBoid.currentCluster.boids.Add(currentBoid);
-                    clusters.Add(currentBoid.currentCluster);
-                }
-            }
+            acceleration += alignmentForce + cohesionForce + seperationForce;
         }
+
+        boid.Velocity += acceleration * Time.deltaTime;
+
+        float speed = boid.Velocity.magnitude;
+        var dir = boid.Velocity / speed;
+        speed = Mathf.Clamp(speed, minMaxSpeed.x, minMaxSpeed.y);
+        boid.Velocity = dir * speed;
+
+        var transform = transforms[boid.index];
+
+        transform.position += boid.Velocity * Time.deltaTime;
+        transform.forward = dir;
+        boid.position = transform.position;
+        boid.forward = dir;
+
+        return boid;
     }
 
-    private async void UpdateClusterCallBack(Cluster cluster)
+    private void OnDrawGizmos ()
     {
-        await Awaitable.MainThreadAsync();
-
-        foreach(var boid in cluster.boids)
-        {
-            transforms[boid.index].position = boid.position;
-        }
+        if ( clusters.Count != 0 )
+            Gizmos.DrawCube(clusters[0].flockPosition, Vector3.one * 5.0f);
     }
 
-    private void UpdateCluster(Cluster cluster, Action<Cluster> updateCallBack)
+    private Vector3 SteerTowards ( Vector3 vector, Vector3 velocity )
     {
-        updateCallBack?.Invoke(cluster);
+        Vector3 v = vector.normalized * minMaxSpeed.y - velocity;
+        return Vector3.ClampMagnitude(v, maxSteerForce);
     }
 }
 
-public class Cluster
+[BurstCompile]
+public struct CalculateClusterData : IJobParallelFor
 {
-    public List<Boid> boids = new();
+    [ReadOnly] public Cluster cluster;
+    public NativeArray<Boid> boids;
+    public float seperationDistance;
+
+    public NativeArray<Vector3> direction;
+    public NativeArray<Vector3> position;
+
+    public void Execute ( int index )
+    {
+        var boid = cluster.boids[index];
+
+        direction[index] += cluster.boids[index].forward;
+        position[index] += cluster.boids[index].position;
+
+        for ( int t = 0; t < cluster.boids.Length; t++ )
+        {
+            var currentBoid = cluster.boids[t];
+            var boidDirection = boid.position - currentBoid.position;
+            var distance = boidDirection.x * boidDirection.x + boidDirection.y * boidDirection.y + boidDirection.z + boidDirection.z;
+
+            if ( distance < seperationDistance * seperationDistance )
+            {
+                boid.avoidanceHeading -= boidDirection / distance;
+            }
+        }
+        boids[index] = boid;
+    }
+}
+
+public struct Cluster
+{
+    public NativeList<Boid> boids;
+
+    public Vector3 flockDirection;
+
+    public Vector3 flockPosition;
+
+    public Cluster ( int _ )
+    {
+        flockDirection = Vector3.zero;
+        boids = new(Allocator.Persistent);
+        flockPosition = Vector3.zero;
+    }
 }
 
 public struct Boid
 {
     public int index;
 
-    public Cluster currentCluster;
+    public Vector3 forward;
+
+    public Vector3 avoidanceHeading;
 
     public Vector3 position;
 
     public Vector3 Velocity;
 
-    public Boid ( int index, Vector3 position, Cluster defaultCluster )
+    public Boid ( int index, Vector3 position, Vector3 forward )
     {
         this.index = index;
         this.position = position;
         Velocity = Vector3.zero;
-        currentCluster = defaultCluster;
+        this.forward = forward;
+        avoidanceHeading = Vector3.zero;
     }
 }
